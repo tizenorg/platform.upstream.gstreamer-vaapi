cacc633b80756c3127731f8c3a74bfa034c50f47
Zhao, Halley <halley.zhao@intel.com>
2014-02-17 04:35:55 +0800

    wayland: disable crop support by vpp
    1080P video got a mismatch 1088 video surface size,
    this mistach will lead to vpp and some failure.
    
    temp disable vpp to avoid the exception for IVI release, I will look
    at the root reason next.

--- gst-libs/gst/vaapi/gstvaapiwindow_wayland.c
+++ gst-libs/gst/vaapi/gstvaapiwindow_wayland.c
@@ -39,6 +39,7 @@
 
 #define DEBUG 1
 #include "gstvaapidebug.h"
+#define WORKAROUND_DISABLE_VPP       1
 
 #define GST_VAAPI_WINDOW_WAYLAND_CAST(obj) \
     ((GstVaapiWindowWayland *)(obj))
@@ -463,6 +464,10 @@ gst_vaapi_window_wayland_render(
         need_vpp = TRUE;
     if (dst_rect->width != window->width || dst_rect->height != window->height)
         need_vpp = TRUE;
+
+    #if WORKAROUND_DISABLE_VPP
+        need_vpp = FALSE;
+    #endif
 
     /* Try to construct a Wayland buffer from VA surface as is (without VPP) */
     if (!need_vpp) {
